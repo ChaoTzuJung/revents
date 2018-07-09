adc4211c194050ab45dc2782434723c4fcaa3fd8
Alan Chao <s110319022@stu.ntue.edu.tw>
2018-07-10 00:29:14 +0800

    initial commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard';
+import NavBar from '../../features/nav/NavBar'
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
